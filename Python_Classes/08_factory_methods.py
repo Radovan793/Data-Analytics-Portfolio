0814c3495afcf24233e9620322e8f620a7f295cc
Radovan793 <radovan.tirol@gmail.com>
2025-08-29 21:19:06 +0200

    Create 08_factory_methods.py

---
+++ Python_Classes/08_factory_methods.py
@@ -0,0 +1,21 @@
+#  Factory methods to create pre-configured car objects.
+
+class Car:
+    def __init__(self, manufacturer):
+        self.manufacturer = manufacturer
+
+    @classmethod
+    def makeBMW(cls):
+        return cls("BMW")
+
+    @classmethod
+    def makeAudi(cls):
+        return cls("Audi")
+
+
+# Example usage
+car1 = Car.makeBMW()
+car2 = Car.makeAudi()
+
+print(car1.manufacturer)
+print(car2.manufacturer)
