ebe1b7789ea0f7394a6af03b21e720cb93228946
Radovan793 <radovan.tirol@gmail.com>
2025-08-29 21:18:20 +0200

    Create 07_class_methods.py

---
+++ Python_Classes/07_class_methods.py
@@ -0,0 +1,12 @@
+# Example of class vs instance methods.
+
+class MyClass:
+    class_variable = "I am static!"
+
+    def instance_method(self):
+        print(MyClass.class_variable)
+
+
+# Example usage
+obj = MyClass()
+obj.instance_method()
